b813c62eb223b018ed10629573c38b25e7571279
lyly <40229016+lylylcn@users.noreply.github.com>
2019-06-27 16:10:22 +0800

    Update

--- tools/tools.js
+++ tools/tools.js
@@ -506,4 +506,24 @@ function ajax(method, url, data){
     xhr.send(data);
 }
 
+function supportCss3(style) {
+    var prefix = ['webkit', 'Moz', 'ms', 'o'],
+        i,
+        humpString = [],
+        htmlStyle = document.documentElement.style,
+        _toHumb = function (string) {
+            return string.replace(/-(\w)/g, function ($0, $1) {
+                return $1.toUpperCase();
+            });
+        };
 
+    for (i in prefix)
+        humpString.push(_toHumb(prefix[i] + '-' + style));
+
+    humpString.push(_toHumb(style));
+
+    for (i in humpString)
+        if (humpString[i] in htmlStyle) return true;
+
+    return false;
+}
